4b6bd62b3cd90449b0f75e0f1388574d64006fe5
LordDanklin <dcurrier585@gmail.com>
2023-02-28 17:30:14 -0500

    completed models and created additional starter files.

--- models/User.js
+++ models/User.js
@@ -1,7 +1,40 @@
-const mongoose = require("mongoose")
+const { Schema, model } = require("mongoose");
 
 
-const userSchema = new mongoose.Schema({
-    username: String,
-    email: String
-})
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        match: [/.+\@.+\..+/],
+        unique: true,
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: "Thought"
+    }
+    ],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+    ],
+},
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+
+    });
+
+userSchema.virtual("friendsCount").get(() => {
+    return this.friends.length;
+});
+
+module.exports = model("User", userSchema);
